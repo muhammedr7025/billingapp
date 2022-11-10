10d187e8bed417b989a5dffccceed8f9b9344fa4
muhammedr7025 <muhammedr7025@gmail.com>
2022-11-10 15:34:53 +0530

    product saving provider with functions

--- lib/provider/customerProvider.dart
+++ lib/provider/customerProvider.dart
@@ -1,6 +1,22 @@
+import 'dart:collection';
+
 import 'package:billingapp/model/customer.dart';
 import 'package:flutter/material.dart';
 
 class CustomerProvider with ChangeNotifier {
   List<Customer> _savedCustomer = [];
+  List<Customer> get savedCustomerList {
+    return [..._savedCustomer];
+  }
+
+  int get customerLength {
+    return _savedCustomer.length;
+  }
+
+  Future<void> saveCustomer(Customer customer) async {
+    _savedCustomer.add(Customer(
+        custName: customer.custName,
+        credit: customer.credit,
+        mobno: customer.mobno));
+  }
 }
