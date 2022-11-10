5b6e49e840ece32829fc612c8d52bdbaf2f7e452
muhammedr7025 <muhammedr7025@gmail.com>
2022-11-10 14:25:39 +0530

    haii

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -1,4 +1,6 @@
-class Product {
+import 'package:flutter/material.dart';
+
+class Product with ChangeNotifier {
   String? productName;
   String? productCode;
   String? stockDate;
---
+++ lib/stockpages/stocklist.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class StockList extends StatefulWidget {
+  const StockList({super.key});
+
+  @override
+  State<StockList> createState() => _StockListState();
+}
+
+class _StockListState extends State<StockList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
