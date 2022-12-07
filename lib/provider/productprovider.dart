724695c2e980c6bf898cc2a2f9b9d8f9a695fd67
muhammedr7025 <muhammedr7025@gmail.com>
2022-12-08 00:23:55 +0530

    temp

--- lib/provider/productprovider.dart
+++ lib/provider/productprovider.dart
@@ -1,3 +1,4 @@
+import 'package:billingapp/provider/firestore_service.dart';
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
@@ -12,101 +13,11 @@ class ProductProvider with ChangeNotifier {
         stockDate: '22/5/2002',
         stockCount: 7),
     Product(
-        productName: 'poco x3',
-        productCode: 'pcx3',
-        price: 27000,
-        stockDate: '22/5/2002',
-        stockCount: 8),
-    Product(
         productName: 'macbook pro',
         productCode: 'mc2',
         price: 229000,
         stockDate: '22/5/2002',
         stockCount: 4),
-    Product(
-        productName: 'iphone 14',
-        productCode: '1p14',
-        price: 144000,
-        stockDate: '22/5/2002',
-        stockCount: 7),
-    Product(
-        productName: 'iwatch 8',
-        productCode: 'iw8',
-        price: 50000,
-        stockDate: '22/5/2002',
-        stockCount: 7),
-    Product(
-        productName: 'iwatch 9',
-        productCode: 'iw9',
-        price: 50000,
-        stockDate: '22/5/2002',
-        stockCount: 7),
-    Product(
-        productName: 'iwatch 10',
-        productCode: 'iw10',
-        price: 50000,
-        stockDate: '22/5/2002',
-        stockCount: 7),
-    Product(
-        productName: 'iphone 8',
-        productCode: 'ip8',
-        price: 50000,
-        stockDate: '22/5/2002',
-        stockCount: 7),
-    Product(
-        productName: 'iphone 4',
-        productCode: 'ip4',
-        price: 50000,
-        stockDate: '22/5/2002',
-        stockCount: 7),
-    Product(
-        productName: 'iphone 5',
-        productCode: 'ip5',
-        price: 50000,
-        stockDate: '22/5/2002',
-        stockCount: 7),
-    Product(
-        productName: 'iphone 6',
-        productCode: 'ip6',
-        price: 50000,
-        stockDate: '22/5/2002',
-        stockCount: 7),
-    Product(
-        productName: 'iphone 7',
-        productCode: 'ip7',
-        price: 50000,
-        stockDate: '22/5/2002',
-        stockCount: 7),
-    Product(
-        productName: 'iphone 9',
-        productCode: 'ip9',
-        price: 50000,
-        stockDate: '22/5/2002',
-        stockCount: 7),
-    Product(
-        productName: 'iphone 10',
-        productCode: 'ip10',
-        price: 50000,
-        stockDate: '22/5/2002',
-        stockCount: 7),
-    Product(
-        productName: 'iphone 11',
-        productCode: 'ip11',
-        price: 50000,
-        stockDate: '22/5/2002',
-        stockCount: 7),
-    Product(
-        productName: 'iphone 12',
-        productCode: 'ip12',
-        price: 50000,
-        stockDate: '22/5/2002',
-        stockCount: 7),
-    Product(
-        productName: 'iphone 13',
-        productCode: 'ip13',
-        price: 50000,
-        stockDate: '22/5/2002',
-        stockCount: 7)
   ];
   List<Product> get savedProductList {
     return _savedProduct;
@@ -120,6 +31,8 @@ class ProductProvider with ChangeNotifier {
         stockCount: product.stockCount,
         stockDate: product.stockDate,
         uniqueId: const Uuid().v4()));
+    final FirestoreService db = FirestoreService();
+    await db.saveProduct(product);
     notifyListeners();
   }
 
